86423b73449d35f8a0c7c5b360af5eb104032b81
Moritz Wilke <wilke@informatik.uni-leipzig.de>
2020-12-14 15:35:16 +0100

    actually removed crt function

--- 13/day13.py
+++ 13/day13.py
@@ -1,4 +1,3 @@
-import math
 from itertools import count
 
 
@@ -7,37 +6,10 @@ def main():
         data = [l.strip() for l in f.readlines()]
 
     departure = int(data[0])
-    busses = [int(b) for b in data[1].split(',') if b.isdigit()]
-    waiting_time = ([(b, b - (departure % b)) for b in busses])
+    buses = [int(b) for b in data[1].split(',') if b.isdigit()]
+    waiting_time = ([(b, b - (departure % b)) for b in buses])
     waiting_time.sort(key=lambda x: x[1])
     print(waiting_time[0][0] * waiting_time[0][1])
-
-
-def mul_inv(a, b):
-    b0 = b
-    x0, x1 = 0, 1
-
-    if b == 1:
-        return 1
-
-    while a > 1:
-        q = a // b
-        a, b = b, a % b
-        x0, x1 = x1 - q * x0, x0
-
-    if x1 < 0:
-        x1 += b0
-
-    return x1
-
-
-def chinese_remainder(mod_remain):
-    product = math.prod([m for m, r in mod_remain])
-    sum = 0
-    for mod, remain in mod_remain:
-        p = product // mod
-        sum += remain * mul_inv(p, mod) * p
-    return sum % product
 
 
 def main2():
